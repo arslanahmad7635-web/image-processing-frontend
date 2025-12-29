d9738afa3049a6c7879f9203ffec3bc57fb5f848
Arslan Ahmad <arslanahmad7635@gmail.com>
2025-12-29 02:31:17 -0800

    Small Change to Home Screen

--- src/Screens/ContactScreen/ContactScreen.jsx
+++ src/Screens/ContactScreen/ContactScreen.jsx
@@ -1,15 +1,51 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar'
-import Feedback from './Components/Feedback'
+
+
 
 function ContactScreen() {
   return (
-    <>
-      <Navbar/>
-      <Feedback/>
+    <section className='w-full flex flex-col items-center justify-start'>
+    
+        <Navbar/>
+        
+        <div className="overflow-hidden flex flex-col mt-22 font-poppins">
+          
+          <div className="text-white p-8 flex flex-col justify-center flex flex-col items-center justify-center">
+
+            <h2 className="text-3xl text-black font-bold mb-3 max-sm:text-2xl">Contact Us</h2>
+
+            <div className='flex flex-col items-center justify-center w-full'>
+              <p className="text-black max-sm:text-sm">Feel free to contact us anytime and share your queries.</p>
+            </div>
+
+          </div>
+          
+          <form className="flex flex-col items-center justify-start mt-4">
+
+            <div className='w-full flex flex-col items-start justify-start'>
+              <label className="block text-sm font-medium text-gray-600 mb-1">Full Name</label>
+              <input type="text" placeholder="Rizwan" className="w-full rounded-sm border border-gray-300 px-4 py-2"/>
+            </div>
+
+            <div className='w-full flex flex-col items-start justify-start mt-8'>
+              <label className="block text-sm font-medium text-gray-600 mb-1">Email Address</label>
+              <input type="email" placeholder="Rizwan@gmail.com" className="w-full rounded-sm border border-gray-300 px-4 py-2 "/>
+            </div>
+
+            <div className='w-full flex flex-col items-start justify-start mt-8'>
+              <label className="block text-sm font-medium text-gray-600 mb-1">Message</label>
+              <textarea rows="5" placeholder="Write your message here..." className="w-full rounded-sm border border-gray-300 px-4 py-2 resize-none"/>
+            </div>
+
+            <button type="submit" className="w-full bg-light hover:bg-primary text-white font-medium py-4 transition mt-10 rounded-sm">Send Message</button>
+            
+          </form>
+
+        </div>
     
     
-    </>
+    </section>
   )
 }
 
--- src/Screens/HomeScreen/Components/herosection.jsx
+++ src/Screens/HomeScreen/Components/herosection.jsx
@@ -1,16 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import GridDistortion from '../../../Components/ReactBitsComponents/GridDistortion'
 import BlurText from '../../../Components/ReactBitsComponents/BlurText'
 import GradientText from '../../../Components/ReactBitsComponents/GradientText'
 import { Link } from 'react-router-dom'
+import { AnimatePresence, motion } from 'motion/react'
 
 
 function HeroSection() {
+
+    const [openStartMenu, setOpenStartMenu] = useState(false);
+
+    useEffect(() => {
+        if (openStartMenu) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, [openStartMenu]);
+
+    
 
   return (
 
     <section className='w-full h-screen flex items-center justify-center relative'>
 
+        <AnimatePresence>
+
+            {
+                openStartMenu && (
+
+                    <motion.div initial={{opacity : 0}} animate={{opacity : 1}} exit={{opacity : 0}} className='w-full h-full flex items-center justify-center backdrop-blur-sm fixed z-20'>
+
+                        <div onClick={() => setOpenStartMenu(false)} className='w-full h-full bg-orange-300 absolute opacity-0'></div>
+                    
+                        <motion.div initial={{opacity : 0, scale : 0.9}} animate={{opacity : 1, scale : 1}} transition={{delay : 0.2}} className='shadow-md'>
+                            <Link to={'/image-enhancement'} className='flex items-center justify-center w-60 h-60 bg-white rounded-lg relative'>
+
+                                <h2 to={'/image-enhancement'} className='text-2xl font-poppins'>Enahance</h2>
+
+                            </Link>
+                        </motion.div>
+                        
+                        <motion.div initial={{opacity : 0, scale : 0.9}} animate={{opacity : 1, scale : 1}} transition={{delay : 0.2}} className='ml-5 shadow-md'>
+                            <Link to={'/image-enhancement'} className='flex items-center justify-center w-60 h-60 bg-white rounded-lg relative'>
+
+                                <h2 to={'/image-analysis'} className='text-2xl font-poppins'>Analyze</h2>
+
+                            </Link>
+                        </motion.div>
+                        
+
+                    </motion.div>
+
+                )
+            }
+
+        </AnimatePresence>
         <div className='w-full h-full absolute bg-linear-to-t from-white to-transparent z-2 pointer-events-none flex flex-col items-center justify-center'>
 
             <BlurText
@@ -23,7 +72,7 @@ function HeroSection() {
 
 
             <div className='mt-4'>
-                <Link to={'/get-started'} className='border-none px-8 py-4 text-white rounded-full text-md bg-linear-to-r from-primary to-light hover:from-light hover:to-primary pointer-events-auto transition duration-300 font-poppins'>Get Started</Link>
+                <button onClick={() => setOpenStartMenu(true)} className='border-none px-8 py-4 text-white rounded-full text-md bg-linear-to-r from-primary to-light hover:from-light hover:to-primary pointer-events-auto transition duration-300 font-poppins'>Get Started</button>
             </div>
 
         </div>
